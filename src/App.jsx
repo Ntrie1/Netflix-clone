33e30ab9b9ae78db3a4e834ba93eee238fc718f3
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-12-15 20:17:45 +0200

    more components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import Row from './components/Row/Row'
+import requests from './request'
 
 function App() {
 
   return (
     <>
-     <Row title='NETFLIX ORIGINAL'/>
+      <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </>
   )
 }
--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,13 +1,50 @@
+import axios from "../../axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original"
+
+
+
 const Row = ({
     title,
-
+    isLargeRow,
+    fetchUrl,
 }) => {
 
-    return(
-        <div>
-            <h1>{title}</h1>
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const reqest = await axios.get(fetchUrl);
+            setMovies(reqest.data.results)
+        }
+
+        fetchData();
+
+    }, [fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className='row_posters' >
+
+                {movies.map((movie) => (
+                    <img 
+                    key={movie.id} 
+                    src={`${base_url}${movie.poster_path}`} 
+                    alt={movie.name}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    />
+                ))}
+
+            </div>
         </div>
-        )
+    )
 }
 
-export default Row
+export default Row;
